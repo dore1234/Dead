215480e1442581b7356cb6a603dad124fa33650f
dore1234 <doregaming111@gmail.com>
2024-12-18 21:55:49 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY EVIL4REAL
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7906641255:AAHCCSxajpbnkpPCBvNrSogn11vC_PNVM50")
+BOT_TOKEN=("8000264467:AAGXm5k7WJ4Cyxh-quZjkuwPc0hDjy5D8oI")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("@Sarkar3009")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1300081723")
+ADMIN_IDS=("1884454860")
 
 
 
